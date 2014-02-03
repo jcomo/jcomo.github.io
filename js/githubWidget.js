ecd28685675699e107b4e5159940bddbf1002f0e
Jonathan Como <jonathan.como@gmail.com>
2014-02-03 15:18:09 -0500

    added failure function for fetching github repos

--- js/githubWidget.js
+++ js/githubWidget.js
@@ -9,18 +9,19 @@
 	$.ajax({
 		url: 'https://api.github.com/users/' + vendor + '/repos',
 		dataType: 'jsonp',
+
 		success: function(results) {
 
 			// Get the 5 most recent repos
 			var data = results.data.sort(byUpdated).slice(0, 5);
 			var repoUrl, repoName, date, lastUpdate = 'unknown';
 
-			$.each(data, function(i, item) {
-				repoName = item.name;
-				repoUrl = item.html_url;
+			$.each(data, function(i, repo) {
+				repoName = repo.name;
+				repoUrl = repo.html_url;
 				
-				if (item.pushed_at) {
-					date = moment(item.pushed_at);
+				if (repo.pushed_at) {
+					date = moment(repo.pushed_at);
 					lastUpdate = date.format("MMMM D");
 				}
 
@@ -37,6 +38,11 @@
 
 				$('.repos').append($repo);
 			});
+		},
+
+		error: function(req, status, err) {
+			$error = '<p>Could not connect to GitHub.</p>';
+			$('.repos').append($error);
 		}
 
 	});
